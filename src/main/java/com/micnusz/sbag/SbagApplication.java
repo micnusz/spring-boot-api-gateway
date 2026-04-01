8b2239eaf3f1ce84af0dd16b7cc7889f9949e18a
Michal <michalnuszkiewiczbusiness@gmail.com>
2026-04-01 12:52:02 +0200

    Initial commit

---
+++ src/main/java/com/micnusz/sbag/SbagApplication.java
@@ -0,0 +1,13 @@
+package com.micnusz.sbag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/micnusz/sbag/SbagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micnusz.sbag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
